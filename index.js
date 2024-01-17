b6a50ecd607aaa463e8329204601e5cce2f25d56
Sahil Mondal <development.sahil@gmail.com>
2024-01-17 23:05:00 +0530

    fix

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import youtubeLink from "./routes/youtubeLink/youtubeLink.js";
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/youtube", youtubeLink);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5699;
 app.listen(PORT, () => {
   console.log(`Server started on port http://localhost:${PORT}/`);
 });
