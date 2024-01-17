f31d67522b1fdc67fc5242df994bd55b29021a35
Sahil Mondal <development.sahil@gmail.com>
2024-01-17 23:07:33 +0530

    cors added

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 import youtubeLink from "./routes/youtubeLink/youtubeLink.js";
-
+const corsOptions = {
+  origin: "http://localhost:3000",
+  // credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.post("/youtube", youtubeLink);
 
